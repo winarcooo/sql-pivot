b92c7a584edd1da3cff2982d403629fdf4c6c8dd
Andy Winarko <winarcooo@gmail.com>
2019-05-20 15:26:16 +0700

    add database

--- .docker/conf/postgres/init.sql
+++ .docker/conf/postgres/init.sql
@@ -1,7 +1,19 @@
-CREATE TABLE users (
-  id SERIAL,
-  age INT,
-  first_name VARCHAR(255),
-  last_name VARCHAR(255),
-  email TEXT
+CREATE TABLE invoices (
+  id SERIAL PRIMARY KEY,
+  full_name VARCHAR(255),
+  email TEXT,
+  item VARCHAR(255),
+  quantity INT,
+  total_price INT
 );
+
+INSERT INTO invoices VALUES (DEFAULT, 'Tommy Bejo', 'Tommybejo@gmail.com', 'barang1', 2, 100000);
+INSERT INTO invoices VALUES (DEFAULT, 'Joko widodo', 'Jokowi@gmail.com', 'barang2', 1, 50000);
+INSERT INTO invoices VALUES (DEFAULT, 'Jusuf kalla', 'jusufkalla@gmail.com', 'barang3', 3, 150000);
+INSERT INTO invoices VALUES (DEFAULT, 'Tommy Bejo', 'Tommybejo@gmail.com', 'barang2', 2, 100000);
+INSERT INTO invoices VALUES (DEFAULT, 'Robert garcia', 'robertgarcia@gmail.com', 'barang2', 2, 100000);
+INSERT INTO invoices VALUES (DEFAULT, 'Prabowo subianto', 'prabowo@gmail.com', 'barang3', 1, 120000);
+INSERT INTO invoices VALUES (DEFAULT, 'Sandiaga Uno', 'sandiuno@gmail.com', 'barang1', 2, 100000);
+INSERT INTO invoices VALUES (DEFAULT, 'Maruf amin', 'amin@gmail.com', 'barang3', 3, 150000);
+
+CREATE extension tablefunc;
--- src/app.js
+++ src/app.js
@@ -1,10 +1,35 @@
-var pgp = require('pg-promise')(/* options */)
+var pgp = require('pg-promise')()
+const express = require('express')
+
+const app = express()
+const port = 3000
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
 var db = pgp('postgres://postgres:postgres@postgres:5432/demo')
 
-db.one('select * from users')
+
+app.get('/', (req, res) => {
+  db.any("select * from invoices")
   .then(function (data) {
-    console.log('DATA:', data.value)
+    var result = data.map(item => item)
+    res.render('index', { data: result });
   })
   .catch(function (error) {
     console.log('ERROR:', error)
   })
+})
+
+app.get('/pivot', (req, res) => {
+  db.any("select * from crosstab('select full_name, item, quantity from invoices order by 1,2','select distinct item from invoices order by 1') as final_result(full_name VARCHAR, barang1 INT, barang2 INT, barang3 INT);")
+  .then(function (data) {
+    var result = data.map(item => item)
+    res.render('pivot', { data: result });
+  })
+  .catch(function (error) {
+    console.log('ERROR:', error)
+  })
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
